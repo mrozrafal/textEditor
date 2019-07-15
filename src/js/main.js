b9389776efec8ab8709a77665b03bd6c640cf7c3
Rafal Mroz <rafalmroz30@gmail.com>
2019-07-15 21:10:56 +0200

    feat:js save and load from local storage

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,49 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+
+
+
+
+
+const editorArea = document.querySelector('.editor__area--js');
+const save = document.querySelector('.editor__buttons-save--js');
+const load =  document.querySelector('.editor__buttons-load--js');
+
+
+
+save.addEventListener('click' , (e) =>{
+    
+    localStorage.setItem('save', editorArea.value);
+})
+
+
+
+
+
+if(localStorage.getItem('save')){
+    editorArea.value=localStorage.getItem('save');
+}
+
+
+
+
+
+
+
+
+load.addEventListener('click', (e) =>{
+    editorArea.value = localStorage.getItem('save');
+    
+    
+})
+
+
+
+
+
+
+
+
+
